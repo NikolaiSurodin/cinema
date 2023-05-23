41d9d6d5747248bae4bb91bcff0953a8d16a72b8
Nikolay <kolya_12.93@mail.ru>
2023-05-23 15:36:13 +0700

    changed public path url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? 'https://cinema-s-test.vercel.app/'
+        ? 'https://cinema-kino.vercel.app/'
         : '/',
     lintOnSave: process.env.NODE_ENV !== 'production',
     runtimeCompiler: true,
